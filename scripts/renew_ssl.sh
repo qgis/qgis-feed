8884faf2427f28b655b48e2fda6f1b6a0daec178
Tim Sutton <tim@kartoza.com>
2021-05-13 09:38:33 +0100

    Fix typo in path

--- scripts/renew_ssl.sh
+++ scripts/renew_ssl.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/usr/local/bin/docker-compose -f /home/web/qgisfeed/docker-compose-production-ssl.yml run certbot renew --dry-run \
-&& /usr/local/bin/docker-compose -f /home/web/qgisfeed/docker-compose-production-ssl.yml kill -s SIGHUP nginx
+/usr/local/bin/docker-compose -f /home/web/qgis-feed/docker-compose-production-ssl.yml run certbot renew --dry-run \
+&& /usr/local/bin/docker-compose -f /home/web/qgis-feed/docker-compose-production-ssl.yml kill -s SIGHUP nginx
