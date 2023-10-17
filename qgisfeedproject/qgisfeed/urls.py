cf807b25cba9ac3e21f6fba22036edff855e74b6
Lova ANDRIARIMALALA <43842786+Xpirix@users.noreply.github.com>
2023-10-17 18:05:21 +0300

    Revert "Init add feed page"
    This reverts commit 3ad4cd1705fae39d677814076ac9906280090d70.

--- qgisfeedproject/qgisfeed/urls.py
+++ qgisfeedproject/qgisfeed/urls.py
@@ -5,5 +5,4 @@ from . import views
 urlpatterns = [
     path('', views.QgisEntriesView.as_view(), name='all'),
     path('manage/', views.feeds_list, name='feeds_list'),
-    path('manage/add', views.feed_entry_add, name='feed_entry_add'),
 ]
--- qgisfeedproject/qgisfeed/views.py
+++ qgisfeedproject/qgisfeed/views.py
@@ -21,9 +21,9 @@ from django.http import HttpResponse, HttpResponseBadRequest
 from django.views import View
 from django.db.models import Q
 from django.contrib.auth.decorators import login_required, user_passes_test
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render
 
-from .forms import FeedEntryFilterForm, FeedItemForm
+from .forms import FeedEntryFilterForm
 from .models import QgisFeedEntry
 from .languages import LANGUAGE_KEYS
 import json
@@ -179,31 +179,3 @@ def feeds_list(request):
               "count": count
             }
     )
-
-
-@staff_required
-def feed_entry_add(request):
-    """
-    View to add a feed entry item
-    """
-    msg = None
-    success = False
-    if request.method == 'POST':
-        form = FeedItemForm(request.POST)
-        if form.is_valid():
-            form.save()
-            success = True
-            return redirect('feeds_list')
-        else:
-            success = False
-            msg = "Form is not valid"
-    else:
-        form = FeedItemForm()
-
-    args = {
-        "form": form,
-        "msg": msg,
-        "success": success,
-
-    }
-    return render(request, 'feeds/feed_item_form.html', args)
