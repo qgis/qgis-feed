1eef562f1a8b965d3458b784d67d7702bc4322ea
Xpirix <xpirixii@gmail.com>
2025-09-25 11:42:43 +0300

    Use resend for email sending

--- qgisfeedproject/qgisfeedproject/settings.py
+++ qgisfeedproject/qgisfeedproject/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
-import os
 import logging
+import os
 
 logger = logging.getLogger(__name__)
 
@@ -23,89 +23,85 @@ SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w*x1d0%17@hd6lx8gq#45#8w(w&a-!vupt41%#^ryyxs3l%hz9'
+SECRET_KEY = "w*x1d0%17@hd6lx8gq#45#8w(w&a-!vupt41%#^ryyxs3l%hz9"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('QGIS_FEED_PROD_URL', '')]
+ALLOWED_HOSTS = [os.environ.get("QGIS_FEED_PROD_URL", "")]
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.gis',
-
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.gis",
     # QGIS Feed applications
-    'qgisfeed',
-
+    "qgisfeed",
     # Dependencies
-    'tinymce',  # HTML field
-    'imagekit',  # Image crop and resize
-
+    "tinymce",  # HTML field
+    "imagekit",  # Image crop and resize
     # User visit
-    'user_visit',
-
+    "user_visit",
     # Webpack
-    'webpack_loader',
+    "webpack_loader",
 ]
 
 # Useful debugging extensions
 if DEBUG:
     INSTALLED_APPS += [
-        'django_extensions',
+        "django_extensions",
     ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'qgisfeed.middleware.QgisFeedUserVisitMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "qgisfeed.middleware.QgisFeedUserVisitMiddleware",
 ]
 
-ROOT_URLCONF = 'qgisfeedproject.urls'
+ROOT_URLCONF = "qgisfeedproject.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ os.path.join(BASE_DIR, "templates")],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                'templates.context_processors.settings_var'
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                "templates.context_processors.settings_var",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'qgisfeedproject.wsgi.application'
+WSGI_APPLICATION = "qgisfeedproject.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME':  os.getenv('QGISFEED_DOCKER_DBNAME', 'qgisfeed'),
-        'USER': os.getenv('QGISFEED_DOCKER_DBUSER', 'docker'),
-        'PASSWORD': os.getenv('QGISFEED_DOCKER_DBPASSWORD', 'docker'),
-        'HOST': os.getenv('QGISFEED_DOCKER_DBHOST', 'postgis'),
-        'PORT': '5432'
+    "default": {
+        "ENGINE": "django.contrib.gis.db.backends.postgis",
+        "NAME": os.getenv("QGISFEED_DOCKER_DBNAME", "qgisfeed"),
+        "USER": os.getenv("QGISFEED_DOCKER_DBUSER", "docker"),
+        "PASSWORD": os.getenv("QGISFEED_DOCKER_DBPASSWORD", "docker"),
+        "HOST": os.getenv("QGISFEED_DOCKER_DBHOST", "postgis"),
+        "PORT": "5432",
     }
 }
 
@@ -115,16 +111,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -132,9 +128,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -146,31 +142,33 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
 
-GEOIP_PATH='/var/opt/maxmind/'
+GEOIP_PATH = "/var/opt/maxmind/"
 
 # This can be specified in settings_local
-MAIN_WEBSITE_URL='https://qgis.org'
+MAIN_WEBSITE_URL = "https://qgis.org"
 
-QGISFEED_FROM_EMAIL = os.environ.get("QGISFEED_FROM_EMAIL", 'noreply')
-EMAIL_BACKEND = os.environ.get("EMAIL_BACKEND", 'django.core.mail.backends.smtp.EmailBackend')
-EMAIL_HOST = os.environ.get("EMAIL_HOST", 'smtp.gmail.com')
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL", "noreply")
+EMAIL_BACKEND = os.environ.get(
+    "EMAIL_BACKEND", "django.core.mail.backends.smtp.EmailBackend"
+)
+EMAIL_HOST = os.environ.get("EMAIL_HOST", "smtp.gmail.com")
 EMAIL_PORT = os.environ.get("EMAIL_PORT", 587)
 EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", True)
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", 'noreply')
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", '')
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "noreply")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
 
 try:
-    from .settings_local import *
-except ImportError as ex:
+    pass
+except ImportError:
     pass
 
 LOGIN_REDIRECT_URL = "/manage"
@@ -179,9 +177,9 @@ LOGOUT_REDIRECT_URL = "/"
 
 # Webpack
 WEBPACK_LOADER = {
-    'DEFAULT': {
-        'BUNDLE_DIR_NAME': 'bundles',
-        'STATS_FILE': os.path.join(os.path.dirname(BASE_DIR), 'webpack-stats.json'),
+    "DEFAULT": {
+        "BUNDLE_DIR_NAME": "bundles",
+        "STATS_FILE": os.path.join(os.path.dirname(BASE_DIR), "webpack-stats.json"),
     }
 }
 
@@ -212,7 +210,9 @@ if SENTRY_DSN and SENTRY_DSN != "":
 
 # Mastodon
 MASTODON_ACCESS_TOKEN = os.environ.get("MASTODON_ACCESS_TOKEN", "")
-MASTODON_API_BASE_URL = os.environ.get("MASTODON_API_BASE_URL", "https://mastodon.social")
+MASTODON_API_BASE_URL = os.environ.get(
+    "MASTODON_API_BASE_URL", "https://mastodon.social"
+)
 
 # Bluesky
 BLUESKY_HANDLE = os.environ.get("BLUESKY_HANDLE", "")
